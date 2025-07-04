64ca7033de78a5bfd39f4cb97120bc3af1c8a488
Maksym <68908852+makc4us@users.noreply.github.com>
2025-07-04 18:36:54 +0300

    Update manual_checks_after_generic_update.sql

--- sql/Oncology Unified Load Stage/Tests/manual_checks_after_generic_update.sql
+++ sql/Oncology Unified Load Stage/Tests/manual_checks_after_generic_update.sql
@@ -1,22 +1,3 @@
---lookup of concept_relationship
-SELECT DISTINCT
-  c1.concept_id, c1.concept_name, c1.vocabulary_id, c1.standard_concept,
-  relationship_id, cr.invalid_reason,
-  c2.concept_id, c2.concept_name, c2.vocabulary_id, c2.standard_concept,
-  CASE
-    WHEN c2.concept_code IN (select concept_code_2 from concept_relationship_manual)
-      THEN 'new rels'
-      ELSE 'old rels' END as comment 
-FROM concept_relationship cr
-  JOIN concept c1
-    ON c1.concept_id = cr.concept_id_1
-  JOIN concept c2
-    ON cr.concept_id_2 = c2.concept_id
---only those that present in concept_manual
-WHERE c1.concept_code IN (SELECT concept_code_1 FROM concept_relationship_manual)
-  AND c1.vocabulary_id IN ('Cancer Modifier', 'SNOMED', 'ICDO3', 'NAACCR', 'CPT4', 'HemOnc', 'LOINC', 'HCPCS')
-;
-
 --lookup of concepts that have mappings to OMOP Genomic
 SELECT DISTINCT 
   c1.concept_id, c1.concept_name, c1.vocabulary_id, c1.standard_concept,
@@ -40,42 +21,69 @@ WHERE c1.concept_code IN (SELECT concept_code_1 FROM concept_relationship_manual
 ORDER BY c1.concept_name, c2.vocabulary_id DESC
 ;
 
---combo check for changes in standardness, valid_start_date, valid_end_date, invalid_reason among concepts
+--lookup of concepts related to Metastasis
 SELECT 
-  c.concept_id, 
-  c.concept_name,
---standard_concept
-  c2.standard_concept as old_standard_concept,
-  c.standard_concept as new_standard_concept,
-  CASE WHEN coalesce(c.standard_concept, '1') = coalesce(c2.standard_concept ,'1')
-    THEN 'S is similar'
-  ELSE 'S was changed' END as standard_check,
---valid_start_date
-  c2.valid_start_date as old_valid_start_date,
-  c.valid_start_date as new_valid_start_date,
-  CASE WHEN c.valid_start_date = c2.valid_start_date
-    THEN 'vSd is similar'
-  ELSE 'vSd was changed' END as vSd_check, 
---valid_end_date 
-  c2.valid_end_date as old_valid_end_date,
-  c.valid_end_date as new_valid_end_date,
-  CASE WHEN c.valid_end_date = c2.valid_end_date
-    THEN 'vEd is similar'
-  ELSE 'vEd was changed' END as vEd_check,
---invalid_reason
-  c2.invalid_reason as old_invalid_reason,
-  c.invalid_reason as new_invalid_reason,
-  CASE WHEN coalesce(c.invalid_reason,'1') = coalesce(c2.invalid_reason,'1')
-    THEN 'IR is similar'
-  ELSE 'IR was changed' END as IR_check
-FROM concept c
-JOIN devv5.concept c2
-  ON c.concept_id = c2.concept_id
+  c1.concept_id, c1.concept_name, c1.vocabulary_id, c1.standard_concept,
+  relationship_id, cr.invalid_reason,
+  c2.concept_id, c2.concept_name, c2.vocabulary_id, c2.standard_concept
+FROM concept_relationship cr
+  JOIN concept c1
+    ON c1.concept_id = cr.concept_id_1
+  JOIN concept c2
+    ON cr.concept_id_2 = c2.concept_id
 --only those that present in concept_manual
-WHERE EXISTS (
-    SELECT 1 FROM concept_manual cm WHERE c.concept_code = cm.concept_code)
-  AND c.vocabulary_id IN ('Cancer Modifier', 'SNOMED', 'ICDO3', 'NAACCR', 'CPT4', 'HemOnc', 'LOINC', 'HCPCS') 
+WHERE c1.concept_code IN (SELECT concept_code_1 FROM concept_relationship_manual)
+  AND concept_id_1 IN (
+    SELECT concept_id_1
+    FROM concept_relationship cr
+    WHERE concept_id_2 = 36769180 )  --Metastasis
+  AND relationship_id IN ('Maps to', 'Maps to value')
 ;
+
+--lookup of concepts that have BOTH Metastasis and Metastasis to [organ].
+--If concept has mappings to Condition + Metastasis, Metastasis to [organ] has to have concept_relationship.invalid_reason = 'D'.
+--Both mappings to Metastasis and Metastasis to [organ] could not be valid.
+WITH multiple_mets AS (
+SELECT 
+  c1.concept_id,
+  ARRAY_AGG(c2.concept_name ORDER BY c2.concept_name) as arr -- Aggregates concept names into an array
+FROM concept_relationship cr
+JOIN concept c1 
+  ON c1.concept_id = cr.concept_id_1
+JOIN concept c2 
+  ON cr.concept_id_2 = c2.concept_id
+WHERE cr.relationship_id = 'Maps to'
+  AND c2.standard_concept = 'S' 
+  AND c1.concept_id IN (
+      SELECT concept_id_1
+      FROM concept_relationship
+      WHERE concept_id_2 = 36769180 --Metastasis
+    )
+GROUP BY c1.concept_id
+HAVING CARDINALITY(ARRAY_AGG(c2.concept_name)) > 1
+   AND 'Metastasis' = ANY(ARRAY_AGG(c2.concept_name))
+   AND EXISTS (
+        SELECT 1
+        FROM UNNEST(ARRAY_AGG(c2.concept_name)) AS name_in_array
+        WHERE name_in_array LIKE 'Metastasis to%'
+    ))
+       
+SELECT
+  c1.concept_id, c1.concept_name, c1.vocabulary_id, c1.standard_concept,
+  relationship_id, cr.invalid_reason,
+  c2.concept_id, c2.concept_name, c2.vocabulary_id, c2.standard_concept
+FROM concept_relationship cr
+  JOIN concept c1
+    ON c1.concept_id = cr.concept_id_1
+  JOIN concept c2
+    ON cr.concept_id_2 = c2.concept_id
+WHERE relationship_id = 'Maps to' AND c2.standard_concept = 'S'
+  --have both Metastasis & Metastasis to concepts
+  AND c1.concept_id IN (select concept_id from multiple_mets)
+  --filter those that we affected
+  AND c1.concept_code IN
+  (SELECT concept_code_1 FROM concept_relationship_manual)
+; 
 
 --check for entities that are present in CONCEPT, but do not have mappings in CONCEPT_RELATIONSHIP (should return null)
 SELECT *
@@ -129,68 +137,6 @@ WHERE (cr.concept_id_1, cr.concept_id_2) IN (SELECT concept_id_1, concept_id_2 F
   AND cr.relationship_id IN ('Maps to', 'Is a') 
 ;
 
---lookup of concepts related to Metastasis
-SELECT 
-  c1.concept_id, c1.concept_name, c1.vocabulary_id, c1.standard_concept,
-  relationship_id, cr.invalid_reason,
-  c2.concept_id, c2.concept_name, c2.vocabulary_id, c2.standard_concept
-FROM concept_relationship cr
-  JOIN concept c1
-    ON c1.concept_id = cr.concept_id_1
-  JOIN concept c2
-    ON cr.concept_id_2 = c2.concept_id
---only those that present in concept_manual
-WHERE c1.concept_code IN (SELECT concept_code_1 FROM concept_relationship_manual)
-  AND concept_id_1 IN (
-    SELECT concept_id_1
-    FROM concept_relationship cr
-    WHERE concept_id_2 = 36769180 )  --Metastasis
-  AND relationship_id IN ('Maps to', 'Maps to value')
-;
-   
---BOTH Metastasis and Metastasis to [organ] exists
-WITH multiple_mets AS (
-SELECT 
-  c1.concept_id,
-  ARRAY_AGG(c2.concept_name ORDER BY c2.concept_name) as arr -- Aggregates concept names into an array
-FROM concept_relationship cr
-JOIN concept c1 
-  ON c1.concept_id = cr.concept_id_1
-JOIN concept c2 
-  ON cr.concept_id_2 = c2.concept_id
-WHERE cr.relationship_id = 'Maps to'
-  AND c2.standard_concept = 'S' 
-  AND c1.concept_id IN (
-      SELECT concept_id_1
-      FROM concept_relationship
-      WHERE concept_id_2 = 36769180 --Metastasis
-    )
-GROUP BY c1.concept_id
-HAVING CARDINALITY(ARRAY_AGG(c2.concept_name)) > 1
-   AND 'Metastasis' = ANY(ARRAY_AGG(c2.concept_name))
-   AND EXISTS (
-        SELECT 1
-        FROM UNNEST(ARRAY_AGG(c2.concept_name)) AS name_in_array
-        WHERE name_in_array LIKE 'Metastasis to%'
-    ))
-       
-SELECT
-  c1.concept_id, c1.concept_name, c1.vocabulary_id, c1.standard_concept,
-  relationship_id, cr.invalid_reason,
-  c2.concept_id, c2.concept_name, c2.vocabulary_id, c2.standard_concept
-FROM concept_relationship cr
-  JOIN concept c1
-    ON c1.concept_id = cr.concept_id_1
-  JOIN concept c2
-    ON cr.concept_id_2 = c2.concept_id
-WHERE relationship_id = 'Maps to' AND c2.standard_concept = 'S'
-  --have both Metastasis & Metastasis to concepts
-  AND c1.concept_id IN (select concept_id from multiple_mets)
-  --filter those that we affected
-  AND c1.concept_code IN
-  (SELECT concept_code_1 FROM concept_relationship_manual)
-; 
-
 --check for wrong valid dates in CONCEPT
 SELECT * FROM concept
 --only those that present in concept_manual
@@ -202,3 +148,27 @@ SELECT * FROM concept_relationship
 --only those that present in concept_manual
 WHERE valid_end_date < valid_start_date
 ;
+
+--lookup of count of concept standardness in dev_onco 
+SELECT 
+  'dev_onco' as comment, 
+  c1.standard_concept as standard_concept_1, 
+  c2.standard_concept as standard_concept_2,
+  count(*)
+FROM dev_onco.concept_ancestor ca
+JOIN dev_onco.concept c1
+  ON ca.ancestor_concept_id = c1.concept_id
+JOIN dev_onco.concept c2
+  ON ca.descendant_concept_id = c2.concept_id
+GROUP BY c1.standard_concept, c2.standard_concept ;
+
+--lookup of count of concept standardness in devv5
+SELECT 
+  'devv5' as comment, c1.standard_concept as standard_concept_1, c2.standard_concept as standard_concept_2,
+  count(*)
+FROM devv5.concept_ancestor ca
+JOIN devv5.concept c1
+  ON ca.ancestor_concept_id = c1.concept_id
+JOIN devv5.concept c2
+  ON ca.descendant_concept_id = c2.concept_id
+GROUP BY c1.standard_concept, c2.standard_concept ;
